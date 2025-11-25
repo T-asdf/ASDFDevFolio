fc0b342a7d42136718c5dd621a9835a624456587
T-asdf <86279096+T-asdf@users.noreply.github.com>
2025-11-25 04:01:07 +0000

    애니메이션 추가

--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="flex justfiy-center items-center h-[90vh] bg-gradient-to-br from-gray-900 to-black">
-        <div className="flex text-gray-100 font-[100] gap-2 text-5xl w-1/2 m-auto items-center justfiy-center tracking-tighter">
+        <div className="animate-fadeInLeft flex text-gray-100 font-[100] gap-2 text-5xl w-1/2 m-auto items-center justfiy-center tracking-tighter">
           개발자 <span className="font-[500]">이서준</span> 입니다
         </div>
       </div>
@@ -12,12 +12,12 @@ export default function Home() {
       <div className="flex flex-col min-h-[10vh]">
         <div className="w-full min-h-[10vh] bg-gradient-to-tr from-gray-900 to-black">
           <div className="flex min-h-[10vh] text-white w-3/4 mx-auto justify-center gap-10">            
-            <div className="flex w-3/4 min-h-[10vh] justify-between text-md font-[400] text-gray-400">
+            <div className="animate-fadeIn flex w-3/4 min-h-[10vh] justify-between text-md font-[200] text-gray-300">
               <Link href="/about">
                 <div className="cursor-pointer">About</div>
               </Link>
 
-              <Link href="/portfolio">
+              <Link href="/project">
                 <div className="cursor-pointer">Project</div>
               </Link>
 
@@ -37,9 +37,9 @@ export default function Home() {
         </div> 
       </div>
 
-      <div className="w-full h-[50vh] bg-gradient-to-br from-gray-900 to-black text-white">
+      {/* <div className="w-full h-[50vh] bg-gradient-to-br from-gray-900 to-black text-white">
         
-      </div>
+      </div> */}
     </>
   );
 }
--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -10,6 +10,35 @@ module.exports = {
       fontFamily: {
         sans: ['Pretendard', 'system-ui', 'sans-serif'],
       },
+
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(40px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+
+        fadeInLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-40px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(40px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+      },
+
+      animation: {
+        fadeIn: "fadeIn 1.0s ease-out forwards",
+        fadeInUp: "fadeInUt 1.0s ease-out forwards",
+        fadeInLeft: "fadeInLeft 1.0s ease-out forwards",
+        fadeInRight: "fadeInRight 1.0s ease-out forwards",
+      },
     },
   },
 
